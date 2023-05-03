c7cd83a0b69ae420785197951f55388bf327c94a
Shani-Koller <121733956+Shani-Koller@users.noreply.github.com>
2023-05-03 20:02:16 +0300

    Update service.js

--- PracticodeClient-main/src/service.js
+++ PracticodeClient-main/src/service.js
@@ -1,36 +1,39 @@
 // import process.env.API_ADDRESS from 'process.env.API_ADDRESS';
 //process.env.API_ADDRESS.defaults.baseURL= 'http://localhost:5030'
 
-process.env.API_ADDRESS.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    (error) => {
-      console.error('An error occurred:', error);
-      return Promise.reject(error);
-    }
-  );
+const apiClient = axios.create({
+  baseURL: process.env.API_ADDRESS,
+});
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
+
 
 export default {
   getTasks: async () => {
-    const result = await process.env.API_ADDRESS.get(`/items`);
+    const result = await apiClient.get(`/items`);
     return result.data;
   },
 
   addTask: async(newToDo)=>{
     console.log('addTask', newToDo)
-    await process.env.API_ADDRESS.post(`/items`, {name:newToDo,isComplete:false});
+    await apiClient.post(`/items`, {name:newToDo,isComplete:false});
     return {};
   },
 
   setCompleted: async(id, isComplete)=>{
     console.log('setCompleted', {id, isComplete});
-    await process.env.API_ADDRESS.put(`/items/${id}`);
+    await apiClient.put(`/items/${id}`);
   },
 
   deleteTask:async(id)=>{
     console.log('deleteTask')
-    await process.env.API_ADDRESS.delete(`/items/${id}`);
+    await apiClient.delete(`/items/${id}`);
   }
 };
 
